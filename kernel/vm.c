67d7778d79390f4dd7e2d9d464464f2e5b62b75c
xsr <1670167385@qq.com>
2023-03-05 15:06:45 +0800

    pteprintout done

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,25 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void walk_print(pagetable_t pagetable, int depth)
+{
+
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V)){
+      uint64 child = PTE2PA(pte);
+      for(int i=0; i<=depth; i++)
+        printf(" ..");
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0)
+        walk_print((pagetable_t)child, depth + 1);
+    } 
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  walk_print(pagetable, 0);
+}
+
